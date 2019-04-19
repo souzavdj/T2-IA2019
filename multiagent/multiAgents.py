7549f190c9a86c7fde2532f11fa8de8d68a77eb7
Vinicius <souza.vdj@gmail.com>
2019-04-19 02:40:07 -0300

    Incluindo o codigo feito pelo professor para ajudar na questao 3

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -333,6 +333,32 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
   """
     Your minimax agent with alpha-beta pruning (question 3)
   """
+  
+  #def minValue(self, gameState, agentIndex, depth, alpha, beta):
+
+  #      v = {'value':float('inf'), 'action':Directions.STOP}
+
+  #      legalMoves = gameState.getLegalActions(agentIndex)
+
+  #      for action in legalMoves:
+
+  #          if action == Directions.STOP: continue
+
+  #          successorGameState = gameState.generateSuccessor(agentIndex, action)
+
+  #          successorMinMax = self.minimax(successorGameState, agentIndex+1, depth, action, alpha, beta)
+
+  #          if v['value'] >= successorMinMax['value']:
+
+  #              v['value'] = successorMinMax['value']
+
+  #              v['action'] = action
+
+  #          if v['value'] <= alpha: return v
+
+  #          beta = min(beta, v['value'])
+
+  #      return v
 
   def getAction(self, gameState):
     """
